e583eb3af2c2d8b7d8195f57d7a23ac0f1882898
Alexandra Samoilova <avsamoilava@gmail.com>
2022-10-16 17:02:59 +0300

    feat: add remove-from-list solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,10 +24,95 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+/* 
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
 }
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+const list = convertArrayToList([3, 1, 2, 3, 4, 5]); */
+
+
+function removeKFromList(l, k) {
+
+  function convertToArray(list) {
+    arr.push(list.value);
+    if (list.next) {
+      convertToArray(list.next)
+    }
+  }
+
+  function indexOf(element) {
+    let current = l;
+    let index = 0;
+
+    while (current) {
+      if (current.value === element) {
+        return index;
+      }
+
+      current = current.next;
+      index++;
+    }
+    return -1;
+  }
+
+  function removeAt(position) {
+    if (position < 0 || position >= l.length) {
+      return null;
+    }
+
+    let current = l;
+
+    if (position === 0) {
+      l = current.next;
+    } else {
+      let prev = null;
+      let index = 0;
+      while (index < position) {
+        prev = current;
+        current = current.next;
+        index++;
+      }
+      prev.next = current.next;
+    }
+    this.length--;
+    console.log(l)
+    return current.value;
+  }
+
+  function remove(element) {
+    removeAt(indexOf(element))
+  }
+
+  
+  let arr = [];
+  convertToArray(l);
+  
+  
+  if (!arr.includes(k)) {
+    return l;
+  } else {
+    remove(k);
+    return removeKFromList(l, k);
+  }
+}
+
 
 module.exports = {
   removeKFromList
